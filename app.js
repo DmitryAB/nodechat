122c1e459fcba8f7383e49eff00210d0da7ad1e7
DmitryAB <dmitriy.generalov@inbox.ru>
2015-03-04 20:30:06 +0400

    Username added

--- app.js
+++ app.js
@@ -3,27 +3,32 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var GUEST_NAME = 'Guest';
+
 app.use(express.static('public'));
 
-
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('a user connected');
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function() {
     console.log('user disconnected');
   });
 
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function(msg) {
     console.log('message: ' + msg);
-    io.sockets.emit('chat message', msg);
+    io.sockets.emit('chat message', (socket.username || GUEST_NAME) + ': ' + msg);
+  });
+
+  socket.on('set name', function(name) {
+    socket.username = name;
   });
 });
 
-http.listen(8080, function(){
+http.listen(8080, function() {
   console.log('listening on *:8080');
 });
 
