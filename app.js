b01ff1003fe201ed5a976f9bd9bac5d24143bd68
DmitryAB <arbaitenmail@gmail.com>
2015-03-04 18:00:49 +0300

    added send form

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-var socket = require(‘socket.io’);
-var express = require(‘express’);
-
+var express = require('express');
 var app = express();
-var io = socket.listen(app.listen(8080));
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
+
